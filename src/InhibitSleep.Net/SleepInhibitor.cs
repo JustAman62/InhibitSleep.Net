9c5b049e827d13ccf193cdf35b84f9aa446a7887
JustAman62 <amanrk@hotmail.co.uk>
2025-08-06 21:58:48 +0100

    Add a a common abstraction to hide OS specific details

---
+++ src/InhibitSleep.Net/SleepInhibitor.cs
@@ -0,0 +1,24 @@
+﻿namespace InhibitSleep.Net;
+
+public class SleepInhibitor : ISleepInhibitor
+{
+    private ISleepInhibitor _inhibitor;
+
+    public SleepInhibitor(string identifier)
+    {
+        if (OperatingSystem.IsMacOS())
+        {
+            _inhibitor = new MacSleepInhibitor(identifier);
+        }
+        else
+        {
+            throw new NotSupportedException(
+                $"InhibitSleep.Net is not supported on the current operating system."
+            );
+        }
+    }
+
+    public void InhibitSleep() => _inhibitor.InhibitSleep();
+
+    public void ReleaseInhibition() => _inhibitor.ReleaseInhibition();
+}
