0fe826bd4511839a233de18bf4c50a7ac9cc4120
JustAman62 <amanrk@hotmail.co.uk>
2025-10-24 21:40:15 +0100

    Add additional docs

--- src/InhibitSleep.Net/SleepInhibitor.cs
+++ src/InhibitSleep.Net/SleepInhibitor.cs
@@ -7,6 +7,13 @@ public class SleepInhibitor : ISleepInhibitor
 {
     private readonly ISleepInhibitor _inhibitor;
 
+    /// <summary>
+    /// Constructs a <see cref="SleepInhibitor"/> which abstracts away platform specific implementations.
+    /// </summary>
+    /// <param name="identifier">
+    /// Identifier that any assertion will be associated with. Should be something like the name of the app requesting
+    /// the OS not to sleep.
+    /// </param>
     public SleepInhibitor(string identifier)
     {
         if (OperatingSystem.IsMacOS())
